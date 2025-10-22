991cf64ee88c8edd4199f61af8e5e9fd1a1eacea
MamankSYQ <abdurrahmansyafiq151123@gmail.com>
2025-10-22 10:33:57 +0700

    Tugas 2

---
+++ TugasNo2_01.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class TugasNo2_01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int jenis, durasi;
+        int total = 0;
+
+        System.out.print("Masukkan jenis kendaraan (1 Mobil, 2 Motor, 0 Keluar) : ");
+        jenis = sc.nextInt();
+        
+        while (jenis != 0) {
+            if (jenis == 1 || jenis == 2){
+                System.out.print("Masukkan durasi : ");
+                    durasi = sc.nextInt();
+
+                if (durasi > 5){
+                    total += 12500;
+                }
+                else if (jenis == 1){
+                    total += durasi * 3000;
+                }
+                else if (jenis == 2){
+                    total += durasi * 2000;
+                }
+            }
+            System.out.print("Masukkan jenis kendaraan (1 Mobil, 2 Motor, 0 Keluar) : ");
+            jenis = sc.nextInt();
+        }
+        System.out.println("Total harganya adalah Rp." + total);
+    }
+}
